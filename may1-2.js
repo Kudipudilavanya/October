fc0b4fec58baca6847b68b05e7dc4984fb019e81
Kudipudilavanya <139426033+Kudipudilavanya@users.noreply.github.com>
2023-11-15 11:41:43 +0530

    Update may1-2.js

--- may1-2.js
+++ may1-2.js
@@ -2,3 +2,42 @@ What is React?
 React, sometimes referred to as a frontend JavaScript framework, is a JavaScript library created by Facebook.
 
 React is a tool for building UI components.
+
+
+  Example 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function Hello(props) {
+  return <h1>Hello World!</h1>;
+}
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(<Hello />);
+
+    How does React Work?
+React creates a VIRTUAL DOM in memory.
+
+Instead of manipulating the browser's DOM directly, React creates a virtual DOM in memory, where it does all the necessary manipulating, before making the changes in the browser DOM.
+
+React only changes what needs to be changed!
+
+React finds out what changes have been made, and changes only what needs to be changed.
+
+
+  What is ES6?
+ES6 stands for ECMAScript 6.
+
+ECMAScript was created to standardize JavaScript, and ES6 is the 6th version of ECMAScript, it was published in 2015, and is also known as ECMAScript 2015.
+
+React uses ES6, and you should be familiar with some of the new features like:
+
+Classes
+Arrow Functions
+Variables (let, const, var)
+Array Methods like .map()
+Destructuring
+Modules
+Ternary Operator
+Spread Operator
